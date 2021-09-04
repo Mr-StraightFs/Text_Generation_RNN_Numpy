612127fb297cb171ef5ffc0931d47f4b339abb4d
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-04 17:16:11 +0100

    Trains the model and generates dinosaur names

--- Text Gen.py
+++ Text Gen.py
@@ -46,3 +46,91 @@ def clip(gradients, maxValue):
     return gradients
 
 
+def model(data_x, ix_to_char, char_to_ix, num_iterations=35000, n_a=50, dino_names=7, vocab_size=27, verbose=False):
+    """
+    Trains the model and generates dinosaur names.
+
+    Arguments:
+    data_x -- text corpus, divided in words
+    ix_to_char -- dictionary that maps the index to a character
+    char_to_ix -- dictionary that maps a character to an index
+    num_iterations -- number of iterations to train the model for
+    n_a -- number of units of the RNN cell
+    dino_names -- number of dinosaur names you want to sample at each iteration.
+    vocab_size -- number of unique characters found in the text (size of the vocabulary)
+
+    Returns:
+    parameters -- learned parameters
+    """
+
+    # Retrieve n_x and n_y from vocab_size
+    n_x, n_y = vocab_size, vocab_size
+
+    # Initialize parameters
+    parameters = initialize_parameters(n_a, n_x, n_y)
+
+    # Initialize loss (this is required because we want to smooth our loss)
+    loss = get_initial_loss(vocab_size, dino_names)
+
+    # Build list of all dinosaur names (training examples).
+    examples = [x.strip() for x in data_x]
+
+    # Shuffle list of all dinosaur names
+    np.random.seed(0)
+    np.random.shuffle(examples)
+
+    # Initialize the hidden state of your LSTM
+    a_prev = np.zeros((n_a, 1))
+
+    # for grading purposes
+    last_dino_name = "abc"
+
+    # Optimization loop
+    for j in range(num_iterations):
+
+        # Set the index `idx` (see instructions above)
+        idx = j % len(examples)
+
+        # Set the input X (see instructions above)
+        single_example = examples[idx]
+        single_example_chars = [ch for ch in single_example]
+        single_example_ix = [char_to_ix[ch] for ch in single_example_chars]
+        X = [None] + single_example_ix
+
+        # Set the labels Y
+        ix_newline = char_to_ix['\n']
+        Y = X[1:] + [char_to_ix["\n"]]
+
+        # Perform one optimization step: Forward-prop -> Backward-prop -> Clip -> Update parameters
+        # learning rate of 0.01
+        curr_loss, gradients, a_prev = optimize(X, Y, a_prev, parameters, learning_rate=0.01)
+
+        # debug statements to aid in correctly forming X, Y
+        if verbose and j in [0, len(examples) - 1, len(examples)]:
+            print("j = ", j, "idx = ", idx, )
+        if verbose and j in [0]:
+            print("single_example =", single_example)
+            print("single_example_chars", single_example_chars)
+            print("single_example_ix", single_example_ix)
+            print(" X = ", X, "\n", "Y =       ", Y, "\n")
+
+        # to keep the loss smooth.
+        loss = smooth(loss, curr_loss)
+
+        # Every 2000 Iteration, generate "n" characters thanks to sample() to check if the model is learning properly
+        if j % 2000 == 0:
+
+            print('Iteration: %d, Loss: %f' % (j, loss) + '\n')
+
+            # The number of dinosaur names to print
+            for name in range(dino_names):
+                # Sample indices and print them
+                sampled_indices = sample(parameters, char_to_ix, seed)
+                last_dino_name = get_sample(sampled_indices, ix_to_char)
+                print(last_dino_name.replace('\n', ''))
+
+            print('\n')
+
+    return parameters, last_dino_name
+
+
