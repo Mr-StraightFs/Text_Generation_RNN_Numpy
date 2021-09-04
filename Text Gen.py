10cb11e5c7f1a0b17ec79a3f150d246fb4d2c780
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-04 16:40:56 +0100

    Gradient Clipping Func

--- Text Gen.py
+++ Text Gen.py
@@ -20,5 +20,29 @@ ix_to_char = { i:ch for i,ch in enumerate(chars) }
 pp = pprint.PrettyPrinter(indent=4)
 pp.pprint(ix_to_char)
 
+# Gradient Clipping Function
+def clip(gradients, maxValue):
+    '''
+    Clips the gradients' values between minimum and maximum.
+
+    Arguments:
+    gradients -- a dictionary containing the gradients "dWaa", "dWax", "dWya", "db", "dby"
+    maxValue -- everything above this number is set to this number, and everything less than -maxValue is set to -maxValue
+
+    Returns:
+    gradients -- a dictionary with the clipped gradients.
+    '''
+    gradients = copy.deepcopy(gradients)
+
+    dWaa, dWax, dWya, db, dby = gradients['dWaa'], gradients['dWax'], gradients['dWya'], gradients['db'], gradients[
+        'dby']
+
+    # Clip to mitigate exploding gradients
+    for gradient in [dWax, dWaa, dWya, db, dby]:
+        np.clip(gradient, -maxValue, maxValue, out=gradient)
+
+    gradients = {"dWaa": dWaa, "dWax": dWax, "dWya": dWya, "db": db, "dby": dby}
+
+    return gradients
 
 
