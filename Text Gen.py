b2169818eb0902e4e44eb6bd2b79613b76788f6b
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-09-04 16:27:07 +0100

    Packege loading and Data Processing

---
+++ Text Gen.py
@@ -0,0 +1,24 @@
+import numpy as np
+from utils import *
+import random
+import pprint
+import copy
+
+# Dataset and Preprocessing
+data = open('dinos.txt', 'r').read()
+data= data.lower()
+chars = list(set(data))
+data_size, vocab_size = len(data), len(chars)
+print('There are %d total characters and %d unique characters in your data.' % (data_size, vocab_size))
+
+chars = sorted(chars)
+print(chars) # The characters are a-z (26 characters) plus the "\n" (or newline character)
+
+# Create a Char dictionary
+char_to_ix = { ch:i for i,ch in enumerate(chars) }
+ix_to_char = { i:ch for i,ch in enumerate(chars) }
+pp = pprint.PrettyPrinter(indent=4)
+pp.pprint(ix_to_char)
+
+
+
